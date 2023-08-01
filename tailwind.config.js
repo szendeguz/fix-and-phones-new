aacddb88f5e8748cbbce5493e5755cf073f08df4
unknown <szucsbendi@gmail.com>
2023-08-01 11:58:38 +0200

    colors and font family added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#181818',
+        light: '#1f1f1f',
+        lighter: '#2f2f2f',
+        accent: '#00bdf7',
+        white: '#e0e0e0',
+      }
+    },
   },
   plugins: [],
 }
