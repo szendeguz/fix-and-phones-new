42454dc7319f3335e2a9725bd7a3cd72eeafa427
unknown <szucsbendi@gmail.com>
2023-08-01 12:28:45 +0200

    nav and footer

---
+++ src/components/Navigation/PageFooter.vue
@@ -0,0 +1,38 @@
+<template>
+    <footer class="bg-light shadow mt-4">
+      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+          <div class="sm:flex sm:items-center sm:justify-between">
+              <a href="#" class="flex items-center mb-4 sm:mb-0">
+                  <img src="../../assets/images/logos/logo.png" class="h-12 sm:h-16 mr-3" alt="Fix and Phones Logo" />
+                  <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Fix and Phones</span>
+              </a>
+              <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-400 sm:mb-0 dark:text-gray-400">
+                  <li>
+                      <a href="#" class="mr-4 hover:underline md:mr-6 ">Szolgáltatásaink</a>
+                  </li>
+                  <li>
+                      <a href="#" class="mr-4 hover:underline md:mr-6">Szervíz</a>
+                  </li>
+                  <li>
+                      <a href="#" class="mr-4 hover:underline md:mr-6 ">Rólunk</a>
+                  </li>
+                  <li>
+                      <a href="#" class="hover:underline">Kapcsolat</a>
+                  </li>
+              </ul>
+          </div>
+          <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" class="hover:underline">Fix and Phones™</a>.</span>
+      </div>
+  </footer>
+  </template>
+  
+  <script>
+  export default {
+      
+  }
+  </script>
+  
+  <style>
+  
+  </style>
