78f96ac842a9ab07d7b5149148ac128176703145
unknown <szucsbendi@gmail.com>
2023-08-16 11:17:14 +0200

    new svgs, new image

--- src/views/Iphone/ModularChangeView.vue
+++ src/views/Iphone/ModularChangeView.vue
@@ -31,7 +31,7 @@ export default {
         headline: 'Mik azok a moduláris hibák?',
         paragraph1: 'Az alaplap felületére forrasztott áramkörök fizikai hatásra, extrém hő hatására vagy túlfeszültség következtében (hibás, vagy nem jó minőségű utángyártott töltő) meghibásodhatnak, elengedhetnek, amelynek következtében eltérő hibákat produkálhat készülékünk.',
         paragraph2: 'Az alaplapi meghibásodások szinte minden esetben megjavíthatóak alaplap csere nélkül is, de ehhez professzionális géppark, igényes munka és több éves tapasztalat szükséges!',
-        img: '../src/assets/images/sections/iphone_battery_section.jpg',
+        img: '../src/assets/images/sections/modular_section.jpeg',
         softwareUpdateImg: '../src/assets/images/sections/software_update_section.jpg',
         dataSavingImg: '../src/assets/images/sections/data_saving_section.jpg',
     }
